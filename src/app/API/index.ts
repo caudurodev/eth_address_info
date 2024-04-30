63030acd1c59bed0781f7d0ea11012bd5de257f9
caudurodev <rod@cauduro.dev>
2024-04-30 12:16:06 +0200

    feat: basic project scaffolding with api request

---
+++ src/app/API/index.ts
@@ -0,0 +1,33 @@
+export const getEthAddressBalance = async (address: string) => {
+    const request = new URL('https://api.etherscan.io/api');
+    request.searchParams.append('module', 'account');
+    request.searchParams.append('action', 'balance');
+    request.searchParams.append('address', address);
+    request.searchParams.append('tag', 'latest');
+    request.searchParams.append('apikey', process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY!);
+    try {
+        const response = await fetch(request.toString());
+        const parsedResponse = await response.json();
+        return parsedResponse.result
+    } catch (e) {
+        return null;
+    }
+}
+
+export const getRecentAddressTransactions = async (address: string) => {
+    const request = new URL('https://api.etherscan.io/api');
+    request.searchParams.append('module', 'account');
+    request.searchParams.append('action', 'txlist');
+    request.searchParams.append('address', address);
+    request.searchParams.append('startblock', '0');
+    request.searchParams.append('endblock', '99999999');
+    request.searchParams.append('sort', 'desc');
+    request.searchParams.append('apikey', process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY!);
+    try {
+        const response = await fetch(request.toString());
+        const parsedResponse = await response.json();
+        return parsedResponse.result
+    } catch (e) {
+        return null;
+    }
+}
